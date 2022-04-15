5b4128b55a74ddb65c2fed908bb55ec09252bd96
Leon <mcenderplayz@gmail.com>
2022-04-15 08:07:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>∞</title>
+<meta name="description" content="Nothing important to see.">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://krass.life/">
+<meta property="og:title" content="I still wonder why you tease me">
+<meta property="og:description" content="Nothing important to see.">
+<meta property="og:image" content="https://krass.life/media/2.PNG">
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://krass.life/">
+<meta property="twitter:title" content="I still wonder why you tease me">
+<meta property="twitter:description" content="Nothing important to see.">
+<meta property="twitter:image" content="https://krass.life/media/2.PNG">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<meta name="author" content="Leon M." />
+<link rel="icon" type="image/png" href="https://www.vectary.com/login/images/favicon.c629af.png" />
+<link href="css/main.css?v=<?php echo rand(101, 999); ?>" rel="stylesheet" />
+<link href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" rel="stylesheet" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+</head>
+    <body oncontextmenu="return false" onselectstart="return false" ondragstart="return false" onload="on()">
+        <div class="loader"></div>
+        <div id="overlay">
+            <div id="main">
+                <div id="content-box2">
+                    <div id="content">
+                        <h1>Welcome!</h1>
+                        <p>
+                            Due to limitations set by various browsers, interaction is needed to unmute the background video.<br />
+                            Please press OK to continue.
+                        </p>
+                        <br />
+                        <button class="w3-btn w3-border" onclick="start()">OK!</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="main">
+            <a href="https://steamcommunity.com/groups/emptytokyo"><div id="name" class="w3-animate-zoom"><img src="./media/Empty.tokyo.png" /></div></a>
+        </div>
+
+        <div id="footer">
+		
+			<p>
+	     		<details id="songname">
+					<summary>Info</summary>
+					<p>Song: Lil Peep - Nuts (feat. Lil Skil)</p>
+					<p><a href="https://open.spotify.com/track/2SabxRgV7VMc1LRHgw0HIo?si=3138f97367104c39">Spotify</a></p>
+					<p><a href="https://www.youtube.com/watch?v=wYyZ3_s662U">Youtube</a></p>
+					<br>
+					<h5><a href="https://twitter.com/messages/3131299636-3131299636?recipient_id=3131299636&text=hello!">Contact</a><i style="color:gray"> ∞ </i>
+					<a href="https://www.durchdacht.me/">Impressum</a><i style="color:gray"> ∞ </i>
+					<a href="https://www.durchdacht.me/">Privacy</a></h5>
+				</details>
+			</p>
+
+
+
+        </div>
+        <video id="vid" autoplay="" loop="" muted>
+            <source src="/videos/LiL PEEP- Nuts (feat. Lil Skil) Music Video.mp4" type="video/mp4" />
+            <script>
+                document.getElementById("vid").volume = 1;
+            </script>
+        </video>
+        <script src="js/app.js?v=<?php echo rand(101, 999); ?>"></script>
+    </body>
+</html>
